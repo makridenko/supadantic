543002b56019a7dd6123ba9b578919f5e1f23f60
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2025-06-04 14:56:30 +0300

    tests for gt, lte in exclude method

--- tests/test_q_set.py
+++ tests/test_q_set.py
@@ -87,6 +87,40 @@ class TestQSet:
         # Testing
         assert actual_q_set == expected_q_set
 
+    def test_exclude_less_than_or_equal(self, model_mock: type['ModelMock']):
+        # Prepare data
+        expected_q_set = QSet(
+            model_class=model_mock,
+            cache=[
+                model_mock(id=3, name='test_name'),
+                model_mock(id=4, name='new_name'),
+            ],
+        )
+
+        # Execution
+        actual_q_set = model_mock.objects.exclude(id__lte=2)
+        actual_q_set._execute()
+
+        # Testing
+        assert actual_q_set == expected_q_set
+
+    def test_exclude_greater_than(self, model_mock: type['ModelMock']):
+        # Prepare data
+        expected_q_set = QSet(
+            model_class=model_mock,
+            cache=[
+                model_mock(id=1, name='test_name'),
+                model_mock(id=2, name='unique_name'),
+            ],
+        )
+
+        # Execution
+        actual_q_set = model_mock.objects.exclude(id__gt=2)
+        actual_q_set._execute()
+
+        # Testing
+        assert actual_q_set == expected_q_set
+
     def test_filters_with_wrong_field(self, model_mock: type['ModelMock']):
         with pytest.raises(QSet.InvalidFilter, match='Invalid filter'):
             model_mock.objects.filter(foo='bar')
