3ea1e083abff480e060c00ca04f49a732fef1b1d
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2025-06-04 14:34:35 +0300

    added support for gt filter

--- supadantic/query_builder.py
+++ supadantic/query_builder.py
@@ -34,6 +34,7 @@ class QueryBuilder:
         self._equal: tuple[tuple[str, Any], ...] = ()
         self._not_equal: tuple[tuple[str, Any], ...] = ()
         self._less_than_or_equal: tuple[tuple[str, Any], ...] = ()
+        self._greater_than: tuple[tuple[str, Any], ...] = ()
         self._insert_data: dict[str, Any] | None = None
         self._update_data: dict[str, Any] | None = None
         self._delete_mode: bool = False
@@ -91,6 +92,18 @@ class QueryBuilder:
         """
 
         return self._less_than_or_equal
+
+    @property
+    def greater_than(self) -> tuple[tuple[str, Any], ...]:
+        """
+        Gets the greater than filters for the query.
+
+        Returns:
+            (tuple[tuple[str, Any], ...]): A tuple of tuples, where each inner tuple contains a field name and
+                                           its desired value for greater than filtering.
+        """
+
+        return self._greater_than
 
     @property
     def insert_data(self) -> dict[str, Any] | None:
@@ -214,6 +227,19 @@ class QueryBuilder:
         """
 
         self._less_than_or_equal += self._dict_to_tuple(data=kwargs)
+
+    def set_greater_than(self, **kwargs) -> None:
+        """
+        Sets the greater than filters for the query.
+
+        This method accepts keyword arguments representing the greater than filters
+        and appends them to the existing set of greater than filters.
+
+        Args:
+            **kwargs: Key-value pairs where keys are field names and values are the values to exclude.
+        """
+
+        self._greater_than += self._dict_to_tuple(data=kwargs)
 
     def set_insert_data(self, data: dict[str, Any]) -> None:
         """
