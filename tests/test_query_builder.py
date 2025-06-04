00516db6612131e9eeff540ae0d825c1fcb100be
Alexander Pozhidaev <pozhsasa090904@gmail.com>
2025-06-04 14:39:26 +0300

    test for gt filter

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -52,6 +52,17 @@ class TestQueryBuilder:
         assert query_builder.less_than_or_equal == (('id__lte', 4), ('age__lte', 18))
         assert query_builder.mode == QueryBuilder.Mode.FILTER_MODE
 
+    def test_greater_than(self):
+        query_builder = QueryBuilder()
+        assert query_builder.greater_than == ()
+
+        query_builder.set_greater_than(id__gt=4)
+        assert query_builder.greater_than == (('id__gt', 4),)
+
+        query_builder.set_greater_than(age__gt=18)
+        assert query_builder.greater_than == (('id__gt', 4), ('age__gt', 18))
+        assert query_builder.mode == QueryBuilder.Mode.FILTER_MODE
+
     def test_insert_data(self):
         query_builder = QueryBuilder()
         assert query_builder.insert_data is None
